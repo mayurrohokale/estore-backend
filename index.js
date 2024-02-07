342990419a742ec1a1c26dbc492f733eaa01a9a3
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-07 19:33:21 +0530

    added routes for users

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const productCategories = require('./routes/productCategories');
 const products = require('./routes/products');
+const users = require('./routes/users');
 const app = express();
 const cors = require('cors');
 const PORT = 5001;
+const  bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/productCategories', productCategories);
 app.use('/products', products);
+app.use('/users', users);
 
 
 const server = app.listen(PORT, ()=> {
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,47 @@
-
 const express = require('express');
 const pool = require('../shared/pool');
 const bcryptjs = require('bcryptjs');
 const users = express.Router();
 
-users.post('/signup', (req, res)=> {
-    try {
-        let firstName =  req.body.firstname;
-        let lastName = req.body.lastname;
-        let email = req.body.email;
-        let  password = req.body.password;
-        let address =  req.body.address;
-        let city = req.body.city;
-        let state = req.body.state;
-        let pin = req.body.pin;
+users.post('/signup', (req, res) => {
+  try {
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let address = req.body.address;
+    let city = req.body.city;
+    let state = req.body.state;
+    let pin = req.body.pin;
+    let email = req.body.email;
+    let password = req.body.password;
 
-        pool.query(`select count(*) as count from users where email like '${email}`),
-        (error, resultCount) =>{
-            if(error){
-                res.status(500).send(error);
-                } else if(resultCount[0] > 0 ){
-                    res.status(200).send('Email alerady exists');
-                }else{
-                    bcryptjs.hash(password,10).then((hashedPassword) => {
-                        const query = 
-                    })
+    pool.query(
+      `select count(*) as count from users where email like '${email}'`,
+      (error, resultCount) => {
+        if (error) {
+          res.status(500).send(error);
+        } else {
+          if (resultCount[0].count > 0) {
+            res.status(200).send('Email already exists');
+          } else {
+            bcryptjs.hash(password, 10).then((hashedPassword) => {
+              const query = `Insert into users (email,firstName,lastName,address,city,state,pin,password)
+                    values
+                    ('${email}','${firstName}','${lastName}','${address}','${city}','${state}','${pin}','${hashedPassword}')`;
+              pool.query(query, (error, result) => {
+                if (error) {
+                  res.status(401).send(error);
+                } else {
+                  res.status(201).send('success');
                 }
-    }     }
-})
+              });
+            });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = users;
