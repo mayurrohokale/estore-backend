9710ebdf50910b091c3e9891f72b094db85adf63
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-09 13:14:43 +0530

    formed user login API

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('../shared/pool');
 const bcryptjs = require('bcryptjs');
 const users = express.Router();
+const jwtoken = require('jsonwebtoken');
 
 users.post('/signup', (req, res) => {
   try {
@@ -18,10 +19,13 @@ users.post('/signup', (req, res) => {
       `select count(*) as count from users where email like '${email}'`,
       (error, resultCount) => {
         if (error) {
-          res.status(500).send({error: error.code, message: error.message});
+          res.status(500).send({
+            error: error.code,
+            message: error.message,
+          });
         } else {
           if (resultCount[0].count > 0) {
-            res.status(200).send({message: 'Email already exists'});
+            res.status(200).send({ message: 'Email already exists' });
           } else {
             bcryptjs.hash(password, 10).then((hashedPassword) => {
               const query = `Insert into users (email,firstName,lastName,address,city,state,pin,password)
@@ -29,9 +33,12 @@ users.post('/signup', (req, res) => {
                     ('${email}','${firstName}','${lastName}','${address}','${city}','${state}','${pin}','${hashedPassword}')`;
               pool.query(query, (error, result) => {
                 if (error) {
-                  res.status(401).send({error: error.code, message: error.message});
+                  res.status(401).send({
+                    error: error.code,
+                    message: error.message,
+                  });
                 } else {
-                  res.status(201).send( {message: 'success'});
+                  res.status(201).send({ message: 'success' });
                 }
               });
             });
@@ -40,7 +47,58 @@ users.post('/signup', (req, res) => {
       }
     );
   } catch (error) {
-    res.status(400).send({error: error.code, message: error.message});
+    res.status(400).send({
+      error: error.code,
+      message: error.message,
+    });
+  }
+});
+
+users.post('/login', (req, res) => {
+  try {
+    let email = req.body.email;
+    let password = req.body.password;
+    pool.query(
+      `select * from users where email like '${email}'`,
+      (error, result) => {
+        if (error) {
+          res.status(500).send({
+            error: error.code,
+            message: error.message,
+          });
+        } else {
+          if (result.length > 0) {
+            bcryptjs
+              .compare(password, result[0].password)
+              .then((compareResult) => {
+                if (compareResult) {
+                  const token = jwtoken.sign(
+                    { id: result[0].id, email: result[0].email },
+                    'estore-secret-key',
+                    { expiresIn: '1h' }
+                  );
+                  res.status(200).send({
+                    token: token,
+                  });
+                } else {
+                  res.status(401).send({
+                    message: `Invalid password.`,
+                  });
+                }
+              });
+          } else {
+            res.status(401).send({
+              message: `User doesn't exist.`,
+            });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    res.status(400).send({
+      error: error.code,
+      message: error.message,
+    });
   }
 });
 
